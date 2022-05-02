224e35be52140c6e64b101ad037387b86ad16fd0
Viktor Stoianov <viktor.web.frontend@gmail.com>
2022-05-02 23:31:32 +0300

    remake tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
  * Implement simple binary search tree according to task description
@@ -13,45 +13,112 @@ class BinarySearchTree {
   }
 
   root() {
-    if (this.nodes.length === 0) {
-      return null;
-    }
-
     return this.treeRoot;
   }
 
   add(data) {
-    if (this.nodes.length === 0) {
-      this.treeRoot = { data: data };
+    if (this.treeRoot === null) {
+      this.treeRoot = new Node(data);
+    } else {
+      let current = this.treeRoot;
+      while (current !== null) {
+        if (current.data > data) {
+          if (current.left === null) {
+            current.left = new Node(data);
+            return;
+          }
+          current = current.left;
+        } else {
+          if (current.right === null) {
+            current.right = new Node(data);
+            return;
+          }
+          current = current.right;
+        }
+      }
     }
-    this.nodes.push(data);
   }
 
   has(data) {
-    return this.nodes.includes(data);
+    return !!this.find(data);
   }
 
   find(data) {
-    if (this.nodes.includes(data)) {
-      return { data: data };
+    if (this.treeRoot === null) {
+      return this.treeRoot;
     } else {
+      let current = this.treeRoot;
+      while (current !== null) {
+        if (current.data > data) {
+          current = current.left;
+        } else if (current.data < data) {
+          current = current.right;
+        } else {
+          return current;
+        }
+      }
       return null;
     }
   }
 
   remove(data) {
-    let index = this.nodes.indexOf(data);
-    if (index >= 0) {
-      this.nodes.splice(index, 1);
+    let parent = this.treeRoot;
+    parent = this.removeNode(parent, data);
+  }
+
+  removeNode(node, data) {
+    if (node.data === data) {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+      const minNode = this.getMin(node.right);
+      node.data = minNode.data;
+      node.right = this.removeNode(node.right, minNode.data);
+      return node;
+    } else {
+      if (node.data > data) {
+        if (node.left === null) {
+          return node;
+        } else {
+          node.left = this.removeNode(node.left, data);
+          return node;
+        }
+      } else {
+        if (node.right === null) {
+          return node;
+        } else {
+          node.right = this.removeNode(node.right, data);
+          return node;
+        }
+      }
+    }
+  }
+
+  getMin(node) {
+    if (node.left !== null) {
+      return this.getMin(node.left);
+    } else {
+      return node;
     }
   }
 
   min() {
-    return Math.min(...this.nodes);
+    let current = this.treeRoot;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
   }
-
   max() {
-    return Math.max(...this.nodes);
+    let current = this.treeRoot;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
   }
 }
 
